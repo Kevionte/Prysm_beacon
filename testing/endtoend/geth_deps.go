1bf421a283c478c310537f1a0bb3cc42fbde8b6a
Kevionte <kevionte94049@gmail.com>
2024-08-22 12:37:55 -0700

    beacon client

---
+++ testing/endtoend/geth_deps.go
@@ -0,0 +1,19 @@
+package endtoend
+
+// This file contains the dependencies required for https://github.com/Kevionte/Go-Sovereign/cmd/geth.
+// Having these dependencies listed here helps go mod understand that these dependencies are
+// necessary for end to end tests since we build go-ethereum binary for this test.
+import (
+	_ "https://github.com/Kevionte/Go-Sovereign/accounts"          // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/accounts/keystore" // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/cmd/utils"         // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/common"            // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/console"           // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/eth"               // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/eth/downloader"    // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/ethclient"         // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/les"               // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/log"               // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/metrics"           // Required for go-ethereum e2e.
+	_ "https://github.com/Kevionte/Go-Sovereign/node"              // Required for go-ethereum e2e.
+)
